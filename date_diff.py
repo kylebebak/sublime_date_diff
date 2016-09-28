4dd8bec4779814a3c8f5bd6d72c2272700c287cb
Kyle Bebak <kylebebak@gmail.com>
2016-09-28 18:58:43 -0500

    added ability to specify various default date formats in sublime-settings

--- date_diff.py
+++ date_diff.py
@@ -11,15 +11,26 @@ from datetime import datetime, timedelta
 settings_filename = "DateDiff.sublime-settings"
 
 
+class PromptDateDiffFmt(sublime_plugin.WindowCommand):
+    def run(self): # invoked before `on_done`, which makes passing args via `self` very convenient
+        self.fmts = sublime.load_settings(settings_filename).get('fmts')
+        self.window.show_quick_panel([f.get('label') for f in self.fmts], self.on_done)
+
+    def on_done(self, index):
+        fmt = self.fmts[index].get('fmt')
+        self.window.run_command('prompt_date_diff_days', {'fmt': fmt})
+
+
 class PromptDateDiffDaysCommand(sublime_plugin.WindowCommand):
-    def run(self):
+    def run(self, fmt=None):
+        self.fmt = fmt
         self.window.show_input_panel('Days:', '', self.on_done, None, None)
 
     def on_done(self, text):
         try:
             days = int(text)
             if self.window.active_view():
-                self.window.active_view().run_command('date_diff', {'days': days})
+                self.window.active_view().run_command('date_diff', {'fmt': self.fmt, 'days': days})
         except ValueError:
             pass
 
@@ -27,9 +38,13 @@ class PromptDateDiffDaysCommand(sublime_plugin.WindowCommand):
 class DateDiffCommand(sublime_plugin.TextCommand):
     FMT = '%a %Y-%m-%d'
 
-    def run(self, edit, days=0):
+    def run(self, edit, fmt=None, days=0):
         settings = sublime.load_settings(settings_filename)
-        fmt = settings.get("fmt") or self.FORMAT
+        try:
+            default_fmt = settings.get('fmts')[0].get('fmt')
+        except:
+            default_fmt = None
+        fmt = fmt or default_fmt or self.FMT
 
         view = self.view
         sel = view.sel()
