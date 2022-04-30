1ecdf21b377d3edb0ec6ac1b29ddec04c9a1a02c
Kyle Bebak <kylebebak@gmail.com>
2022-04-30 09:32:01 -0500

    Format with black

--- README.md
+++ README.md
@@ -1,16 +1,15 @@
 # DateDiff
+
 A [Sublime Text](http://www.sublimetext.com/) plugin for inserting the current date ± some number of days.
 
 - [Installation](#installation)
 - [Usage](#usage)
 - [License](#license)
 
-
 ## Installation
-
 
 ## Usage
 
-
 ## License
+
 This code is licensed under the [MIT License](https://opensource.org/licenses/MIT).
--- date_diff.py
+++ date_diff.py
@@ -12,38 +12,42 @@ settings_filename = "DateDiff.sublime-settings"
 
 
 class PromptDateDiffFmt(sublime_plugin.WindowCommand):
-    def run(self): # invoked before `on_done`, which makes passing args via `self` very convenient
-        self.fmts = sublime.load_settings(settings_filename).get('fmts')
-        self.window.show_quick_panel([f.get('label') for f in self.fmts], self.on_done)
+    def run(
+        self,
+    ):  # invoked before `on_done`, which makes passing args via `self` very convenient
+        self.fmts = sublime.load_settings(settings_filename).get("fmts")
+        self.window.show_quick_panel([f.get("label") for f in self.fmts], self.on_done)
 
     def on_done(self, index):
         if index < 0:  # e.g. user presses escape
             return
-        fmt = self.fmts[index].get('fmt')
-        self.window.run_command('prompt_date_diff_days', {'fmt': fmt})
+        fmt = self.fmts[index].get("fmt")
+        self.window.run_command("prompt_date_diff_days", {"fmt": fmt})
 
 
 class PromptDateDiffDaysCommand(sublime_plugin.WindowCommand):
     def run(self, fmt=None):
         self.fmt = fmt
-        self.window.show_input_panel('Days:', '', self.on_done, None, None)
+        self.window.show_input_panel("Days:", "", self.on_done, None, None)
 
     def on_done(self, text):
         try:
             days = int(text)
             if self.window.active_view():
-                self.window.active_view().run_command('date_diff', {'fmt': self.fmt, 'days': days})
+                self.window.active_view().run_command(
+                    "date_diff", {"fmt": self.fmt, "days": days}
+                )
         except ValueError:
             pass
 
 
 class DateDiffCommand(sublime_plugin.TextCommand):
-    FMT = '%a %Y-%m-%d'
+    FMT = "%a %Y-%m-%d"
 
     def run(self, edit, fmt=None, days=0):
         settings = sublime.load_settings(settings_filename)
         try:
-            default_fmt = settings.get('fmts')[0].get('fmt')
+            default_fmt = settings.get("fmts")[0].get("fmt")
         except:
             default_fmt = None
         fmt = fmt or default_fmt or self.FMT
@@ -51,5 +55,5 @@ class DateDiffCommand(sublime_plugin.TextCommand):
         view = self.view
         sel = view.sel()
         dt = datetime.now() + timedelta(days=days)
-        for region in sel: # handle multiple selection regions
+        for region in sel:  # handle multiple selection regions
             view.insert(edit, region.begin(), dt.strftime(fmt))
